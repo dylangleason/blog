ec15d8b47b675164c50e1fabfc4af3abc269db1e
Dylan Gleason <dgleason8384@gmail.com>
2026-04-08 21:35:25 -0700

    Refactor about page using flat-pages

--- haunt.scm
+++ haunt.scm
@@ -1,40 +1,13 @@
-(use-modules (haunt asset)
-	     (haunt artifact)
-	     (haunt builder blog)
+(use-modules (haunt builder blog)
 	     (haunt builder atom)
 	     (haunt builder assets)
+	     (haunt builder flat-pages)
 	     (haunt html)
 	     (haunt post)
 	     (haunt reader commonmark)
 	     (haunt site)
 	     (srfi srfi-19)
 	     (srfi srfi-42))
-
-(define (about-page site posts)
-  (serialized-artifact
-   "about.html"
-   (with-layout
-    theme
-    site
-    "About"
-    `((h1 "Hello")
-      (p "My name is Dylan Gleason and I am a software engineer.
-For the most part, I work as a backend web developer and have written
-web services for a variety of companies. These days, I mostly write
-code in Go or Python, with the occasional JavaScript thrown in for
-good measure.")
-      (p "Outside of work, I enjoy learning programming languages and new
-technologies that I don't get to use in my day-to-day, as well as
-starting (though rarely completing) programming projects that aren't
-related to web development.")
-      (p "Some other hobbies of mine include video gaming, particularly retro
-gaming and the emulation thereof, as well as digital audio synthesis
-and making electronic music. I also enjoy reading — these days mostly high
-fantasy and history — and discovering new recipes that I can cook for
-(inflict upon?) my family. For better or (more likely) worse, I am
-also a fan of the Portland Trail Blazers. Go "
-	 (a (@ (href "//youtu.be/0L0Axk8d0QM")) "Rip City!"))))
-   sxml->html))
 
 (define (stylesheet ref)
   `(link (@ (rel "stylesheet")
@@ -133,21 +106,22 @@ also a fan of the Portland Trail Blazers. Go "
     (span "Built w/ "
 	  (a (@ (href "//dthompson.us/projects/haunt.html")) "Haunt"))))
 
+(define (layout site title body)
+  `((doctype "html")
+    (html
+     (@ (lang "en"))
+     ,(head title site)
+     (body
+      (div (@ (class "container"))
+	   ,nav
+	   (main
+	    (@ (class "main"))
+	    ,body)
+	   ,footer)))))
+
 (define theme
   (theme #:name "main"
-	 #:layout
-	 (lambda (site title body)
-	   `((doctype "html")
-	     (html
-	      (@ (lang "en"))
-	      ,(head title site)
-	      (body
-	       (div (@ (class "container"))
-		    ,nav
-		    (main
-		     (@ (class "main"))
-		     ,body)
-		    ,footer)))))
+	 #:layout layout
 	 #:post-template post-template
 	 #:collection-template collection-template
 	 #:pagination-template pagination-template))
@@ -160,8 +134,8 @@ also a fan of the Portland Trail Blazers. Go "
       #:builders (list (blog #:theme theme
 			     #:post-prefix "/posts"
 			     #:posts-per-page 10)
-		       about-page
 		       (atom-feed)
 		       (atom-feeds-by-tag)
+		       (flat-pages "pages" #:template layout)
 		       (static-directory "css")
 		       (static-directory "js")))
