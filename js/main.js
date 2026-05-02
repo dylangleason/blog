147b65bb9abfe3efcd8d6f5c7627f8403aa3467a
Dylan Gleason <dgleason8384@gmail.com>
2026-05-01 21:44:31 -0700

    Remove unused variable in JS file.

--- js/main.js
+++ js/main.js
@@ -4,37 +4,36 @@
   if (theme) {
     document.documentElement.classList.add(theme)
   }
-  
-  document.addEventListener("DOMContentLoaded", e => {
+
+  document.addEventListener("DOMContentLoaded", _ => {
     // Configure hamburger menu on-click event listener
     const hamburgerMenu = document.querySelector(".nav-toggle")
-    hamburgerMenu.addEventListener("click", e => {
+    hamburgerMenu.addEventListener("click", _ => {
       const links = document.querySelector(".links")
       const isOpen = links.classList.toggle("open")
       hamburgerMenu.setAttribute("aria-expanded", isOpen)
     })
 
     // Configure theme toggle glyph and on-click event listener for changing theme
-    const getSystemTheme = () => 
-	    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+    const getSystemTheme = () =>
+      window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
 
     const toggleTheme = (currentTheme) =>
-	    currentTheme === "dark" ? "light" : "dark"
+      currentTheme === "dark" ? "light" : "dark"
 
     const toggleButton = document.querySelector(".theme-toggle")
-    const toggleKnob = document.querySelector(".theme-toggle::after")
 
     let currentTheme = localStorage.getItem("theme")
     if (!currentTheme) {
       currentTheme = getSystemTheme()
     }
 
-    toggleButton.addEventListener("click", e => {
+    toggleButton.addEventListener("click", _ => {
       const docElement = document.documentElement
       if (!currentTheme) {
-	      currentTheme = getSystemTheme()
+        currentTheme = getSystemTheme()
       } else {
-	      docElement.classList.remove(currentTheme)
+        docElement.classList.remove(currentTheme)
       }
       currentTheme = toggleTheme(currentTheme)
       docElement.classList.add(currentTheme)
